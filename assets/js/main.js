248434c8d86630631d2b334c2dbf87b8d5ec0be4
felipecabesssa <felipesoares100@hotmail.com>
2021-11-08 19:36:02 -0300

    CSS and JS from testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,7 +92,7 @@ modalCloses.forEach((modalClose) =>{
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portifolio__container', {
+let swiperPortfolio = new Swiper('.portifolio__container', {
     cssMode: true,
     loop: true,
 
@@ -108,7 +108,22 @@ let swiper = new Swiper('.portifolio__container', {
 
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
 
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
